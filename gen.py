66f006556514628527b30a808871c6f1061ce397
Christopher King <G.nius.ck@gmail.com>
2015-03-15 17:19:03 -0400

    Generalize

--- gen.py
+++ gen.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from random import uniform
 from math import sin, pi
-alpha = uniform(0.1,pi-0.2)
-beta  = uniform(0.1,pi-alpha-0.1)
+bounds = 0.1
+alpha = uniform(bounds,pi-2*bounds)
+beta  = uniform(bounds,pi-alpha-2*bounds)
 gamma = pi-alpha-beta
 c=uniform(0,2048)
 a=c*sin(alpha)/sin(gamma)
@@ -13,5 +14,5 @@ b≈%s
 c≈%s
 alpha≈%sradians≈%sdegrees
 beta≈%sradians≈%sdegrees
-gamma≈%sradians≈%sdegress
+gamma≈%sradians≈%sdegrees
 """ % (a, b, c, alpha, alpha/pi*180, beta, beta/pi*180, gamma, gamma/pi*180)
