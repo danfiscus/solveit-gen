216abf16aa8b15eb1d65f02289bb1d8ab7ed04c3
Christopher King <G.nius.ck@gmail.com>
2015-03-15 15:51:07 -0400

    Made corrections

--- gen.py
+++ gen.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from random import uniform
 from math import sin, pi
 alpha = uniform(0,pi)
@@ -7,10 +8,10 @@ c=uniform(0,2048)
 a=c*sin(alpha)/sin(gamma)
 b=c*sin(beta)/sin(gamma)
 print """
-a=%s
-b=%s
-c=%s
-alpha=%sradians=%sdegrees
-beta=%sradians=%sdegrees
-gamma=%sradians=%sdegress
+a≈%s
+b≈%s
+c≈%s
+alpha≈%sradians≈%sdegrees
+beta≈%sradians≈%sdegrees
+gamma≈%sradians≈%sdegress
 """ % (a, b, c, alpha, alpha/pi*180, beta, beta/pi*180, gamma, gamma/pi*180)
