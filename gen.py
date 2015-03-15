9c40e00a281a1ebfcff5e26b10112a0e92d7225b
Christopher King <G.nius.ck@gmail.com>
2015-03-15 15:40:00 -0400

    Fixed

--- gen.py
+++ gen.py
@@ -1,8 +1,8 @@
 from random import uniform
 from math import sin, pi
-alpha = uniform(0,2*pi)
-beta  = uniform(0,2*pi-alpha)
-gamma = 2*pi-alpha-beta
+alpha = uniform(0,pi)
+beta  = uniform(0,pi-alpha)
+gamma = pi-alpha-beta
 c=uniform(0,2048)
 a=c*sin(alpha)/sin(gamma)
 b=c*sin(beta)/sin(gamma)
